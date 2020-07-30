7ba57b4904ef981d179e94442b7b02bc36f0b631
MoveAngel <moveangel@minaproject.tech>
2020-07-30 18:51:30 +0000

    pylint: auto fixes

--- corona_virus.py
+++ corona_virus.py
@@ -9,26 +9,28 @@ from covid import Covid
 from userbot import CMD_HELP
 from userbot.events import register
 
+
 @register(outgoing=True, pattern="^.coronavirus(?: |$)(.*)")
 async def corona(event):
     covid = Covid()
     data = covid.get_data()
     country = event.pattern_match.group(1)
     country_data = get_country_data(country, data)
-    output_text = "" 
+    output_text = ""
     for name, value in country_data.items():
         output_text += "`{}`: `{}`\n".format(str(name), str(value))
     await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+
 
 def get_country_data(country, world):
     for country_data in world:
         if country_data["country"] == country:
             return country_data
     return {"Status": "No information yet about this country!"}
-    
-    
+
+
 CMD_HELP.update({
-        "covid": 
+    "covid":
         ".coronavirus <country>\
           \nUsage: Get an information about data covid-19 in your country.\n"
-    })
+})
